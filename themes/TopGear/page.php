710995a721a738033f8e1ef068e7299c7ddf6f40
Your Name <you@example.com>
2020-03-17 17:16:45 +0200

    New version

--- themes/TopGear/page.php
+++ themes/TopGear/page.php
@@ -1,28 +1,40 @@
-<?php get_header(); ?>
-<main id="content">
-<?php if ( have_posts() ) : while ( have_posts() ) :
-				the_post();
-				if (get_the_ID() == 10) {
-					get_template_part( '', 'second' );
-				} else {
-					get_template_part( '', 'page' );	
-				}
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package surfing
+ */
 
+get_header();
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
 
-<div class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
-</div>
-</article>
-<?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
-<?php endwhile; endif; ?>
-</main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
--- themes/TopGear/single.php
+++ themes/TopGear/single.php
@@ -1,13 +1,37 @@
-<?php get_header(); ?>
-<main id="content">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
-<?php endwhile; endif; ?>
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package surfing
+ */
 
-<footer class="footer">
-<?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
-</main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			the_post_navigation();
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
